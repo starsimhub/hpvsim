fa7907d0ee2fe76bdfa612c21e2adbeee8f0e1f1
jamiecohen <jamie.alex.cohen@gmail.com>
2025-05-20 09:35:05 -0700

    starting to port in starsim framework

--- hpvsim/__init__.py
+++ hpvsim/__init__.py
@@ -1,40 +1,24 @@
-# This ensures SCIRIS_NUM_THREADS is utilized
+from .version import __version__, __versiondate__, __license__
+
+from .hpv import *
+from .connectors import *
+from .distributions import *
+from .interventions import *
+from .analyzers import *
+from .utils import *
+
+# Assign the root folder
 import sciris as sc
 
-# Import HPVsim
-from .version import __version__, __versiondate__, __license__
-from .settings      import *
-from .defaults      import *
-from .misc          import *
-from .parameters    import *
-from .utils         import *
-from .plotting      import *
-from .base          import *
-from .people        import *
-from .population    import *
-from .interventions import *
-from .immunity      import *
-from .analysis      import *
-from .sim           import *
-from .run           import *
-from .calibration   import *
+root = sc.thispath(__file__).parent
+# data = root/'data'
 
-# Import the version and print the license unless verbosity is disabled, via e.g. os.environ['HPVSIM_VERBOSE'] = 0
-if settings.options.verbose:
-    print(__license__)
+# Import the version and print the license
+print(__license__)
 
-# Import data and check
-from . import data
-if not data.check_downloaded():
-    try:
-        data.quick_download(init=True)
-    except Exception as E1:
-        try:
-            print(f'Quick download failed ({str(E1)}), trying manual download ...')
-            data.download_data(serial=True)
-        except:
-            errormsg = f"Warning: couldn't download data:\n\n{sc.traceback()}\nProceeding anyway..."
-            print(errormsg)
-
-# Set the root directory for the codebase
-rootdir = sc.thispath(__file__).parent
+# Double-check key requirements -- should match setup.py
+sc.require(
+    ["starsim>=2.2.0", "sciris>=3.1.6", "pandas>=2.0.0", "stisim", "scipy"],
+    message=f"The following dependencies for vagisim {__version__} were not met: <MISSING>.",
+)
+del sc  # Clean up namespace
