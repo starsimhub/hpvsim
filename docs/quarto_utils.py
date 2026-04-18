48c9d903fba73575d332b31d4ea908e05d847a35
Cliff Kerr <questionably@gmail.com>
2026-04-17 22:27:50 -0400

    fix styles

--- docs/quarto_utils.py
+++ docs/quarto_utils.py
@@ -9,31 +9,17 @@ import subprocess
 import sciris as sc
 import hpvsim as hpv
 
-default_folders = ['tutorials']
-temp_patterns = ['**/my-*.*', '**/example*.*']
-temp_items = []
-
-
 def run(cmd):
     """Verbose version of subprocess.run."""
     sc.printgreen(f'\n> {cmd}\n')
     return subprocess.run(cmd, check=True, shell=True)
 
 
-@sc.timer('Update version')
 def update_version(pkg=hpv):
-    sc.heading('Updating docs version number...')
-    filename = '_variables.yml'
-    data = dict(version=pkg.__version__, versiondate=pkg.__versiondate__)
-    orig = sc.loadyaml(filename)
-    if data != orig:
-        sc.saveyaml(filename, data)
-        print('Version updated to:', data)
-    else:
-        print('Version already correct:', orig)
+    sc.heading('Updating version...')
+    return sc.saveyaml('_variables.yml', dict(version=pkg.__version__, versiondate=pkg.__versiondate__))
 
 
-@sc.timer('Build API docs')
 def build_api_docs():
     sc.heading('Building API documentation...')
     return run('python -m quartodoc build')
@@ -75,37 +61,15 @@ def build_interlinks():
     return run('python -m quartodoc interlinks')
 
 
-@sc.timer('Clean outputs')
-def clean_outputs(folders=None, sleep=3, patterns=None):
-    """Clear temporary files produced during notebook runs."""
-    sc.heading('Cleaning outputs ...')
-    if folders is None:
-        folders = default_folders
-    if patterns is None:
-        patterns = temp_patterns
-    filenames = sc.dcp(temp_items)
-    for pattern in patterns:
-        for folder in folders:
-            filenames += sc.getfilelist(folder=folder, pattern=pattern, recursive=True)
-    if len(filenames):
-        print(f'Deleting: {sc.newlinejoin(filenames)}\nin {sleep} seconds')
-        sc.timedsleep(sleep)
-        for filename in filenames:
-            sc.rmpath(filename, verbose=True, die=False)
-    else:
-        print('No files found to clean')
-
-
 if __name__ == '__main__':
+
     if 'pre' in sys.argv:
         sc.heading('Starting Quarto docs build', divider='★')
         update_version()
         build_api_docs()
         customize_aliases()
         build_interlinks()
-    elif 'post' in sys.argv:
-        clean_outputs()
     elif len(sys.argv) > 1:
-        raise ValueError(f'Argument must be "pre" or "post", not {sys.argv}')
+        raise ValueError(f'Currently only "pre" is supported, not {sys.argv}')
     else:
-        raise ValueError('Run with pre or post as argv')
+        raise ValueError('Run with pre as argv')
