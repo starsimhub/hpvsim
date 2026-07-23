408ff25221828aa42b5ea0ab3fa3914ff35cf7d9
RHull-IDM <ryan.hull@gatesfoundation.org>
2026-07-23 11:32:59 -0700

    bumped version date

--- hpvsim/version.py
+++ hpvsim/version.py
@@ -5,5 +5,5 @@ Version and license information.
 __all__ = ['__version__', '__versiondate__', '__license__']
 
 __version__ = '3.0.0'
-__versiondate__ = '2026-07-16'
+__versiondate__ = '2026-07-24'
 __license__ = f'HPVsim {__version__} ({__versiondate__}) — © 2023-2026 by the Gates Foundation'
